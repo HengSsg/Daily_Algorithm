4d0602382a0489631527837f6b904d5cdf04dc89
HyungSeok Park <dnflekf2748@gmail.com>
2024-11-21 16:08:14 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 두 정수 A와 B 입력 받기
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        // A와 B의 합 출력
+        System.out.println(A + B);
+
+        scanner.close();
+    }
+}
